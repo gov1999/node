85865a18599043ed4e1c8339f41d3e5129bb16e6
govinda1999 <gspatel61199@gmail.com>
2020-01-17 00:33:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const http = require('http');
+var port = 8000;
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello from Nodejs!!\n');
+});
+
+server.listen(port);
+console.log(`Server listening on port ${port}`);
